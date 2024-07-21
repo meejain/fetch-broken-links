0847794ff343729207ffe5386d12931bdbc11c11
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-07-22 00:57:42 +0530

    Initial code

--- crawl.js
+++ crawl.js
@@ -1,3 +1,46 @@
+const { JSDOM } = require('jsdom');
+
+const brokenurls = [];
+
+function getURLsfromHTML(htmlBody, baseURL) {
+    var newBaseURL = null;
+    const urls = [];    
+    const dom = new JSDOM(htmlBody);
+    dom.window.document.querySelectorAll('a').forEach((a) => {
+        const href = a.getAttribute('href');
+        if (!href) {
+            return;
+        }
+        if ((href.slice(0, 1) === '/') || (href.slice(0, 1) === '#')) {
+            //relative path
+            try {
+                if (baseURL.slice(-1) === '/') {
+                    newBaseURL = baseURL.slice(0, -1);
+                } else {
+                    newBaseURL = baseURL;
+                }
+                console.log(`Checking Crawling ${newBaseURL}${href}`);
+                const urlobj = new URL(`${newBaseURL}${href}`);
+                urls.push(urlobj.href);
+            } catch (err) {
+                console.log(`Error with relative URL - - - > ${err.message}`);
+                brokenurls.push(`${newBaseURL}${href}`);
+            }
+        } else {
+            //absolute path
+            try {
+                console.log(`Checking Crawling ${href}`);
+                const urlobj = new URL(href);
+                urls.push(urlobj.href);
+            } catch (err) {
+                console.log(`Error with absolute URL - - - >  ${err.message}`);
+            }
+        }
+    });
+    return urls;
+}
+
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString);
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -8,5 +51,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsfromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -21,3 +21,45 @@ test('normalizeURL capitals', () => {
     const actual = normalizeURL(input);
     expect(actual).toBe(expected);
 });
+
+test('getURLsfromHTML absolute', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href="https://blog.boot.dev/path/">Boot.dev blog</a>
+    </body>
+    </html>
+    `;
+    const inputBaseURL = "https://blog.boot.dev/path/";
+    const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.boot.dev/path/"];
+    expect(actual).toEqual(expected);
+});
+
+test('getURLsfromHTML relative', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href="/path/">Boot.dev blog</a>
+    </body>
+    </html>
+    `;
+    const inputBaseURL = "https://blog.boot.dev";
+    const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.boot.dev/path/"];
+    expect(actual).toEqual(expected);
+});
+
+test('getURLsfromHTML invalid', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href="invalid">Invalid URL</a>
+    </body>
+    </html>
+    `;
+    const inputBaseURL = "https://blog.boot.dev";
+    const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL);
+    const expected = [];
+    expect(actual).toEqual(expected);
+});
